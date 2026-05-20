a6a89a61b369b67ed6351cb22a00ea4bd6734f15
Andi15034 <agunung1503@gmail>
2026-05-19 22:21:19 -0400

    String upper case

---
+++ activity1.py
@@ -0,0 +1,21 @@
+
+class IOSsting():
+
+
+    def __init__(self):
+        self.str1 = ""
+
+
+    def get_String(self):
+        self.str1 = input("Enter a string: ")
+
+
+    def print_String(self):
+        print("Result is :", self.str1.upper())
+
+
+str1 = IOSsting()
+
+
+str1.get_String()
+str1.print_String()
